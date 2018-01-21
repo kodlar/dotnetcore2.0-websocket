dcfe65772fc32fe7ec883dee1f5bb51d072e5b7d
kodlar <ogankeskiner@gmail.com>
2018-01-22 01:50:23 +0300

    kodlamaya devam ediyoruz....

---
+++ Infrastructure/ConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Infrastructure
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        //private readonly string connectionString = WebConfigurationManager.ConnectionStrings["MyCon"].ConnectionString; 
+        //string connectionString = WebConfigurationManager.AppSettings["StorageConnectionString"];
+        //string conString = Microsoft.Extensions.Configuration.ConfigurationExtensions.GetConnectionString(this.Configuration, "DefaultConnection");
+        public IDbConnection GetConnection()
+        {
+            IDbConnection db = new SqlConnection("connection");
+            db.Open();
+            return db;
+             
+        }
+    }
+}
---
+++ Infrastructure/Dapper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Infrastructure
+{
+    public static class Dapper
+    {
+        //public static IEnumerable<T> Query<T>(this IDbConnection cnn, string sql, object param = null, SqlTransaction transaction = null, bool buffered = true);
+    }
+}
